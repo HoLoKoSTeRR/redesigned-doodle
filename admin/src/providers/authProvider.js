250dcfb0cc8dfbe54f294966694795eb9e861110
KhrapavitskiyOleg <olegas-2017@yandex.by>
2022-04-14 21:19:59 +0300

    updated providers

--- admin/src/providers/authProvider.js
+++ admin/src/providers/authProvider.js
@@ -1,4 +1,3 @@
-import { AUTH_GET_PERMISSIONS } from "react-admin";
 export default {
   // called when the user attempts to log in
   login: async ({ username, password }) => {
@@ -26,6 +25,26 @@ export default {
     localStorage.removeItem("auth");
     return Promise.resolve();
   },
+  signup: async ({ username, password }) => {
+    const request = new Request(
+      "https://arcane-anchorage-00164.herokuapp.com/api/admin/signup",
+      {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: new Headers({ "Content-Type": "application/json" }),
+      }
+    );
+    try {
+      const response = await fetch(request);
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error(response.statusText);
+      }
+      const auth = await response.json();
+      localStorage.setItem("auth", JSON.stringify(auth));
+    } catch {
+      throw new Error("Network error");
+    }
+  },
   // called when the API returns an error
   checkError: (error) => {
     const status = error.status;
@@ -42,7 +61,7 @@ export default {
   },
   // called when the user navigates to a new location, to check for permissions / roles
   getPermissions: () => {
-    const role = localStorage.getItem('role');
-    return role ? Promise.resolve(role) : Promise.resolve('guest');
+    const role = localStorage.getItem("role");
+    return role ? Promise.resolve(role) : Promise.resolve("guest");
   },
 };
